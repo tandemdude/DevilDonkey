9017fa9535dbe2dd9d23b3bec96be751fa110705
tandemdude <43570299+tandemdude@users.noreply.github.com>
2019-05-25 22:08:57 +0100

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,30 @@
+# Bot requested by DevilDonkey
+# Coded by github u/tandemdude
+# https://github.com/tandemdude
+import discord
+from discord.ext import commands
+import time
+import json
+
+extensions = ['cogs.OnReadyCog', 'cogs.PurgeCog', 'cogs.CustomCommandsCog', 'cogs.AnnouncementsCog', 'cogs.SetupCog', 'cogs.WelcomeCog']
+config_file = 'config.json'
+
+with open(config_file) as json_file:  
+    config = json.load(json_file)
+
+prefix = config['prefix']
+token = 'NTc2NDI2NjUyNTUzNDQ1NDA3.XOBNCQ.kFBTdFJWZPjsLgilAHZOcHspDXk'
+
+bot = commands.Bot(command_prefix=prefix)
+
+
+def run_bot():
+    if len(extensions) != 0:
+        for ext in extensions:
+            bot.load_extension(ext)
+    bot.run(token)
+
+
+while True:
+    run_bot()
+    time.sleep(5)
