d8eb1da4604be479b43547f57f527bfbe6783aba
tandemdude <43570299+tandemdude@users.noreply.github.com>
2019-05-29 13:51:50 +0100

    Removing token from script, added TOKEN.INI

--- bot.py
+++ bot.py
@@ -4,27 +4,36 @@
 import discord
 from discord.ext import commands
 import time
+import configparser
 import json
 
+# Sets up parser and reads the file containing the bot token
+parser = configparser.ConfigParser()
+parser.read('TOKEN.INI')
+
+# List of all extensions to be loaded
 extensions = ['cogs.OnReadyCog', 'cogs.PurgeCog', 'cogs.CustomCommandsCog', 'cogs.AnnouncementsCog', 'cogs.SetupCog', 'cogs.WelcomeCog', 'cogs.GiveawayCog']
 config_file = 'config.json'
 
+# Opens configuration file
 with open(config_file) as json_file:  
     config = json.load(json_file)
 
+# Declares the bot prefix and token, taking values from files
 prefix = config['prefix']
-token = 'YOUR TOKEN GOES HERE'
+token = parser['DEFAULT']['token']
 
-bot = commands.Bot(command_prefix=prefix)
+# bot = commands.Bot(command_prefix=prefix)
 
-
+# Main function creates bot, loads extensions and runs the bot
 def run_bot():
+	bot = commands.Bot(command_prefix=prefix)
     if len(extensions) != 0:
         for ext in extensions:
             bot.load_extension(ext)
     bot.run(token)
 
-
+# Keeps the bot alive 
 while True:
     run_bot()
     time.sleep(5)
